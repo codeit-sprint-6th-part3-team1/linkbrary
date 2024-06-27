59d957ec018d566c242d5fa49788770b37d7379a
이정윤 <gramy159@naver.com>
2024-06-27 16:31:54 +0900

    링크추가 완,검색바&인풋 피드백

--- src/components/component(example)/InputBox/index.tsx
+++ src/components/component(example)/InputBox/index.tsx
@@ -1,52 +1,66 @@
 import React, { useState } from 'react';
 import styles from '@/components/component(example)/InputBox/style.module.scss';
-
+import Logo from '@public/input.svg';
+import Image from 'next/image';
 
 interface Input {
-    email: string;
-    password: string;
+  id: string;
 }
+
 const InputBox: React.FC = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [status, setStatus] = useState<string>('default');
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
+    setStatus('writing');
   };
 
   const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
+    setStatus('writing');
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setStatus('submitted');
     console.log('Email:', email);
     console.log('Password:', password);
   };
 
   return (
-    <div className={styles.inputcontainer}>
-        <form className={styles.container} onSubmit={handleSubmit}>
-            <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={handleEmailChange}
-            required
-            placeholder="내용 입력"
-            className={styles.input}
-            />
+    <form className={styles.container} onSubmit={handleSubmit}>
+      <div className={styles.inputcontainer}>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+          placeholder="내용 입력"
+          required
+          className={styles.inputemail}
+        />
 
-            <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-            required
-            className={styles.input}
-            />
-        </form>
-    </div>
+        <Image 
+          src={Logo}
+          width={20}
+          height={20}
+          alt="password logo"
+          className={styles.logo}
+        />
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+          required
+          className={styles.inputpw}
+        />
+        {status === 'submitted' && <p className={styles.message}>내용 작성했음</p>}
+        {status === 'submitted' && <p className={styles.error}>내용을 다시 작성해주세요</p>}
+      </div>
+    </form>
   );
 };
 
