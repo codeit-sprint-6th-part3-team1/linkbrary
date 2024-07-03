8af97998b79a0321bb5e746851ed698e376c3b36
greenyun2 <cys4875@naver.com>
2024-07-03 14:10:42 +0900

    feat: #7 카드 컴포넌트 완성

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,11 +1,20 @@
 import Footer from '@/components/Footer';
 import Gnb from '@/components/Gnb';
-
+import Card from '@/components/Card';
+const cardData = [
+  {
+    id: 1,
+    content: 'Lorem ipsum dolor sit amet consectetur. Metus amet habitant nunc consequat. Tldkd',
+    createdAt: 'Wed Jun 26 2024 17:48:18 GMT+0900',
+    updatedAt: 'Wed Jun 26 2024 17:48:18 GMT+0900',
+  },
+];
 export default function page() {
   return (
     <>
       <Gnb isLogin />
       <Footer />
+      <Card content={cardData[0].content} createdAt={cardData[0].createdAt} updatedAt={cardData[0].updatedAt} />
     </>
   );
 }
