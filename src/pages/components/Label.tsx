f141686c21df306f489297fe897e423d07864caa
greenyun2 <cys4875@naver.com>
2024-07-06 16:58:15 +0900

    fix: 충돌 해결

--- src/pages/components/Label.tsx
+++ src/pages/components/Label.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 interface LabelProps {
-<<<<<<< HEAD
   children: React.ReactNode;
   className: string;
   htmlFor: string;
@@ -13,14 +12,6 @@ const Label = ({ children, className, htmlFor }: LabelProps) => {
       {children}
     </label>
   );
-=======
-  children: string;
-  className: string;
-}
-
-const Label = ({ children, className }: LabelProps) => {
-  return <label className={className}>{children}</label>;
->>>>>>> a246fcbaf6d9013f2ef70341543812f83b4e5a56
 };
 
 export default Label;
